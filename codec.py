30926bdad474a093a71f59f8622298ac5a69e573
Tarcadia <tarcadia@qq.com>
2023-03-13 01:21:48 +0800

    :bug: debug

--- codec.py
+++ codec.py
@@ -7,19 +7,18 @@ from Crypto.Cipher import AES
 cryptos = AES.new(b"JESAGFISBKUYDGWE", AES.MODE_ECB) ## HARD CODED FOR TEST ONLY!!!
 
 def recv(conn: socket):
-    _lh = conn.recv(1)
-    _ll = conn.recv(1)
+    _lh = ord(conn.recv(1))
+    _ll = ord(conn.recv(1))
     _len = _lh * 0xFF + _ll
     _recv = conn.recv(_len)
     while len(_recv) < _len:
-        _recv += conn.recv(len(_recv) - _len)
+        _recv += conn.recv(_len - len(_recv))
     _recv = cryptos.decrypt(decodebytes(_recv))
     return _recv
 
 def send(conn: socket, data: bytes):
     _send = encodebytes(cryptos.encrypt(data))
-    _len = len(_send)
-    assert _len <= 0xFFFF
-    _lh, _ll = _len // 256, _len % 256
-    conn.send(bytes([_lh, _ll]) + _send)
+    assert len(_send) <= 0xFFFF
+    _len = len(_send).to_bytes(2, 'big')
+    conn.send(_len + _send)
     return
